94943016ab8930bf757f3800b99d1711980dc2b0
sankarlingaa <sankar.a1910@gmail.com>
2023-10-03 14:57:35 +0530

    added 3 model code

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,21 @@
 print("adding all the data processing code related fraud detection")
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return c
+
+def multiplication(a,b):
+    d=a*b
+    return d
+
+print("Multiplication Function")
+
+
+def division(a,b)
+    e = a/b
+    return a
+
+print("Division Function")
